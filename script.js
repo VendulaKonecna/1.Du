7949e3d3e02a5b4f1a3a9c1e57e36290ba43a413
Vendula Konečná <VendaKonecna@gmail.com>
2024-10-31 17:49:12 +0100

    add methods

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let firstName = prompt("Napiš své křestní jméno, bez diakritiky:")
 let surname = prompt("Napiš své přijmení, bez diakritiky:")
 
-surname = surname.trim().slice(0, 5).toLowerCase()
-firstName = firstName.trim().slice(0,3).toLowerCase()
+surname = surname.trim().slice(0, 5).toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").padEnd(5, '0')
+firstName = firstName.trim().slice(0,3).toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").padEnd(3, '0')
 console.log(surname)
 console.log(firstName)
 
